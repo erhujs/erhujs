7c08a398033951de71bc50c685edb26d8ccd99f2
switer <guankaishe@gmail.com>
2016-05-15 22:24:53 +0800

    simple proxy server

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
 
 	mainWindow.loadURL('file://' + __dirname + '/index.html')
 
-	mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
 
 	mainWindow.on('close', function () {
 
--- server/proxy.js
+++ server/proxy.js
@@ -1,21 +1,75 @@
 const http = require('http')
+const express = require('express')
+const net = require('net')
+const url = require('url')
 
-db.allDocs({include_docs: true}).then(function (result) {
-	console.log(result)
-})
-module.exports = function () {
+
+function fixedHeaders(headers) {
+    var hds = {}
+    Object.keys(headers).forEach(function (name) {
+        var value = headers[name]
+        name = name.replace(/(^|-)([a-z])/g, function (m, s, w) {
+            return (s || '') + w.toUpperCase()
+        })
+        hds[name] = value
+    })
+    return hds
+}
+
+function proxy() {
+	/**
+	 * Handle proxy request
+	 */
 	const server = http.createServer((req, res) => {
-		res.write('ok')
-		res.end()
+		var urlObj = url.parse(req.url)
+		var host = req.headers.host
+		var port = url.parse(host).port || 80
+		var path = urlObj.path || '/'
+		var method = req.method
+
+		urlObj[host] = host
+		console.log('Request', {
+			host,
+			port,
+			method,
+			path: url.format(urlObj),
+			headers: req.headers
+		})
+		// emit request to UI
+		// var request = parseRequest(req)
+		// pass through
+		var proxy = http.request({
+			host,
+			port,
+			method,
+			path: url.format(urlObj),
+			headers: req.headers
+		}, (proxyRes) => {
+			// remote ip
+			// proxyRes.connection.remoteAddress
+			console.log('Response from', proxy.connection.remoteAddress)
+			proxyRes.on('data', (data) => {
+				// emit response
+				res.write(data)
+			})
+			proxyRes.on('end', () => {
+				console.log('on end')
+				// emit response end
+				res.end()
+			})
+		})
+		proxy.end()
+		// proxy.on('connect', (req, cltSocket, head) => {
+		// })
 	})
 	var port = 8888
-	server.listen(port, ()=>{
-		db.post({
-			message: 'server start on port ' + port,
-			time: new Date
-		}, function (err, res) {
-			console.log(err, res)
-		})
+	server.listen(port, () => {
 		console.log('Proxy server listen on', port)
 	})
 }
+module.exports = proxy
+
+
+
+
+
