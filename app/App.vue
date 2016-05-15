a49a440d0609ea4261496c2d67468b2e666eea48
xiaokai <kexiaokai@gmail.com>
2016-05-15 23:50:51 +0800

    [ADD] add vue and webpack build

---
+++ app/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <c-header></c-header>
+    <div>body</div>  
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'c-header': Header
+  },
+  props: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+    
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ app/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    {{title}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'c-header',
+  props: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+      title: 'erhu header'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="stylus">
+.header
+  border 1px solid #000
+</style>
