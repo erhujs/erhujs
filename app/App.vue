ee17d3f16197d8e353fbdb9359014887566047a7
switer <guankaishe@gmail.com>
2016-05-18 14:55:09 +0800

    proxy request events

--- app/App.vue
+++ app/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div>
-    <c-header></c-header>
-    <c-menu></c-menu>
-    <section class="container">
-      <c-panel :width="panel.left">
-        <ul class="req-list">
-          <li>
-            <c-item :req="reqHead" index="#"></c-item>
-          </li>
-          <li v-for="it in reqList">
-            <c-item :req="it"></c-item>  
-          </li>
-        </ul>
-      </c-panel>
-      <c-split></c-split>
-      <c-panel :width="panel.right">
-        <span>Content: <br>{{resContent}}</span>
-      </c-panel>
-    </section>
-    <c-footer></c-footer>
-    <mdl-checkbox :checked.sync="checked">Checkbox</mdl-checkbox>
-    <mdl-button v-mdl-ripple-effect colored raised>Ripple Effect</mdl-button>
-  </div>
+	<div>
+		<c-header></c-header>
+		<c-menu></c-menu>
+		<section class="container">
+			<c-panel :width="panel.left">
+				<ul class="req-list">
+					<li>
+						<c-item :req="reqHead" index="#"></c-item>
+					</li>
+					<li v-for="it in reqList">
+						<c-item :req="it"></c-item>
+					</li>
+				</ul>
+			</c-panel>
+			<c-split></c-split>
+			<c-panel :width="panel.right">
+				<span>Content: <br>{{resContent}}</span>
+			</c-panel>
+		</section>
+		<c-footer></c-footer>
+		<mdl-checkbox :checked.sync="checked">Checkbox</mdl-checkbox>
+		<mdl-button v-mdl-ripple-effect colored raised>Ripple Effect</mdl-button>
+	</div>
 </template>
 
 <script>
@@ -37,59 +37,57 @@ import Item from './components/Item.vue'
 const ipcRenderer = electron.ipcRenderer
 
 export default {
-  name: 'App',
-  components: {
-    'c-header': Header,
-    'c-menu': Menu,
-    'c-split': Split,
-    'c-panel': Panel,
-    'c-footer': Footer,
-    'c-item': Item,
-  },
-  props: {
-  },
-  computed: {
-  },
-  data () {
-    return {
-      reqHead: config.reqHead,
-      reqList: [],
-      panel: {
-        left: 50,
-        right: 50
-      },
-      resContent: '',
-      checked: false
-    }
-  },
-  ready () {
-    ipcRenderer.on('request', (event, request) => {
-      console.log(event, request)
-      this.reqList.push({
-        host: request.host,
-        path: request.path,
-        server: request.server
-      })
-    })
-    ipcRenderer.on('response', (event, response) => {
-      console.log(event, response)
-      this.resContent = response.data.toString()
-    })
-  },
-  methods: {
-    
-  }
+	name: 'App',
+	components: {
+		'c-header': Header,
+		'c-menu': Menu,
+		'c-split': Split,
+		'c-panel': Panel,
+		'c-footer': Footer,
+		'c-item': Item,
+	},
+	props: {
+	},
+	computed: {
+	},
+	data () {
+		return {
+			reqHead: config.reqHead,
+			reqList: [],
+			panel: {
+				left: 50,
+				right: 50
+			},
+			resContent: '',
+			checked: false
+		}
+	},
+	ready () {
+		ipcRenderer.on('connect', (event, req) => {
+			this.reqList.push({
+				host: req.host,
+				path: req.path,
+				server: req.server
+			})
+		})
+		ipcRenderer.on('responseEnd', (event, req, res) => {
+			this.resContent = res.data.toString()
+		})
+	},
+	methods: {
+
+	}
 }
 </script>
 
 <style lang="stylus">
 .req-list
-  margin 0
-  padding 0
-  li
-    list-style none
+	margin 0
+	padding 0
+	li
+		list-style none
 .container
-  height 200px
-  display flex
-  border 1px solid #eee
+	height 200px
+	display flex
+	border 1px solid #eee
 </style>
