4a9906dd2b8883c45496b8bc73750803d8a9ee33
xiaokai <kexiaokai@gmail.com>
2016-05-20 15:06:17 +0800

    style: move front end ui to gui dir

--- test/develop/index.sh
+++ test/develop/index.sh
@@ -1,5 +1,5 @@
 
-proxy='http://localhost:8888/'
+proxy='http://localhost:8008/'
 
 curl --proxy $proxy 'http://jandan.net/'
 curl --proxy $proxy 'http://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js'
