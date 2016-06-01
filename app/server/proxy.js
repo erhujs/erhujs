5ed4770fad2b429b83954e2451b4294144bed8dc
switer <guankaishe@gmail.com>
2016-06-01 23:46:04 +0800

    remove TLS detect function

--- app/server/proxy.js
+++ app/server/proxy.js
@@ -420,18 +420,6 @@ function getProxy() {
     https: process.env['https_proxy'] || process.env['HTTPS_PROXY']
   }
 }
-/*
-* Detect TLS from first bytes of data
-* Inspired from https://gist.github.com/tg-x/835636
-* used heuristic:
-* - an incoming connection using SSLv3/TLSv1 records should start with 0x16
-* - an incoming connection using SSLv2 records should start with the record size
-*   and as the first record should not be very big we can expect 0x80 or 0x00 (the MSB is a flag)
-* - everything else is considered to be unencrypted
-*/
-function isTLS(head) {
-  return head[0] == 0x16 || head[0] == 0x80 || head[0] == 0x00
-}
 function headersStringify(headers) {
   return Object.keys(headers).map(function (k) {
     return k + ': ' + headers[k]
